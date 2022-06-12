e469f56d77e71e1d4ab4416f48fee1d537e2fc2d
JongRok-Lee <lrrghdrh@naver.com>
2022-06-12 20:19:12 +0900

    Module

---
+++ module/include/module.hpp
@@ -0,0 +1,11 @@
+#ifndef SLAM_CLASSMAT_HPP
+#define SLAM_CLASSMAT_HPP
+
+// #include "opencv2/opencv.hpp"
+// #include "Eigen/Dense"
+// #include "Eigen/Geometry"
+// #include "pangolin/pangolin.h"
+// #include "ceres/ceres.h"
+// #include "glog/logging.h"
+
+#endif //SLAM_CLASSMAT_HPP
